df3bb68426972f1325f7186936d9a1aba3c05a6d
SatoShun <shun.sato1@gmail.com>
2015-08-19 11:44:56 +0000

    Add javadoc comments

--- rx-receiver/src/main/java/com/github/satoshun/rxreceiver/RxReceiver.java
+++ rx-receiver/src/main/java/com/github/satoshun/rxreceiver/RxReceiver.java
@@ -7,23 +7,35 @@ import android.support.annotation.CheckResult;
 
 import rx.Observable;
 
+/** RxReceiver is factory methods for creating {@link Observable}. */
 public class RxReceiver {
+
+    /**
+     * Create a {@link Observable} that emits on broadcast {@link Intent}.
+     * specify action for {@link IntentFilter}.
+     */
     @CheckResult
     public static Observable<Intent> registerBroadcastReceiver(Context context, String action) {
         return registerBroadcastReceiver(context, new IntentFilter(action));
     }
 
+    /** Create a {@link Observable} that emits on broadcast {@link Intent}. */
     @CheckResult
     public static Observable<Intent> registerBroadcastReceiver(Context context, IntentFilter filter) {
         return Observable.create(
                 new BroadcastReceiverOnSubscribe(context.getApplicationContext(), filter));
     }
 
+    /**
+     * Create a {@link Observable} that emits on local broadcast {@link Intent}.
+     * specify action for {@link IntentFilter}.
+     */
     @CheckResult
     public static Observable<Intent> registerLocalBroadcastReceiver(Context context, String action) {
         return registerLocalBroadcastReceiver(context, new IntentFilter(action));
     }
 
+    /** Create a {@link Observable} that emits on local broadcast {@link Intent}. */
     @CheckResult
     public static Observable<Intent> registerLocalBroadcastReceiver(Context context, IntentFilter filter) {
         return Observable.create(
